ac4c5b20b89213acaf81090060502830b21e1da1
vyom rastogi <rastogi122503@gmail.com>
2025-07-10 23:28:04 +0530

    fix firebase env issue

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -7,13 +7,17 @@ import {
 } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore"; 
 
-const servicekey = process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string;
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
 
 let app: App;
 
 if(getApps().length === 0) {
     app = initializeApp({
-        credential: cert(servicekey)
+        credential: cert({
+            projectId: serviceAccount.project_id,
+            clientEmail: serviceAccount.client_email,
+            privateKey: serviceAccount.private_key.replace(/\\n/g, "\n"),
+        })
     });
 }else{
     app = getApp();
